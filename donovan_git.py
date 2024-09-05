25132e232d1e7a502fc34d49c4f352db2160cd31
plalleras0 <gdonovan761@gmail.com>
2024-09-05 15:30:51 -0600

    firstandlastname

---
+++ donovan_git.py
@@ -0,0 +1,7 @@
+print ("what is your firstname and lastname:")
+firstName =input()
+lastName =input()
+input = ("first_name , lastname")
+fullname= F'Hello, {firstName} {lastName}!'
+print(fullname)
+
