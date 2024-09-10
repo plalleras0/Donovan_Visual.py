34f22ce2e8ca3f952234e4293a247fbda9fcc7e6
plalleras0 <gdonovan761@gmail.com>
2024-09-10 14:04:28 -0600

    game rock paper sicciers

---
+++ rock_paper_siciors.py
@@ -0,0 +1,22 @@
+import random
+options = (['rock', 'paper', 'siccers'])
+computer_ch = random.choice(['rock', 'paper', 'siccers'])
+
+while True :
+    user_ch= input("choose a world of this list : 'rock', 'paper', 'siccers': ")
+    
+    if user_ch == "quit":
+        break
+
+    if user_ch not in options:
+        print("not allow")
+        continue
+
+    if user_ch == computer_ch:
+        print("tie")
+   
+    elif (user_ch == "rock" and computer_ch == "scissors") or (user_ch == "paper" and computer_ch == "rock") or (user_ch == "scissors" and computer_ch == "paper"):
+        print("Won")
+
+    else:
+        print("lose")
