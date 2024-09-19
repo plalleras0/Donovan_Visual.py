b7565cd5d62ba317acdf38151d9bea98c3aad24c
plalleras0 <gdonovan761@gmail.com>
2024-09-19 15:23:35 -0600

    RPS

--- RockPaperSiccers.py
+++ RockPaperSiccers.py
@@ -1,22 +1,29 @@
 import random
 options = (['rock', 'paper', 'siccers'])
 computer_ch = random.choice(['rock', 'paper', 'siccers'])
+print(computer_ch)
 
-while True :
-    user_ch= input("choose a world of this list : 'rock', 'paper', 'siccers': ")
+def mesage(texto):
+    print (f"********** {texto}. *****")
+
+def game():
+    while True :
+        user_ch= input("choose a world of this list : 'rock', 'paper', 'siccers': ")
+        
+        if user_ch == "quit":
+            break
+
+        if user_ch not in options:
+            mesage("Not allow")
+            continue
+
+        if user_ch == computer_ch:
+            mesage("tie")
     
-    if user_ch == "quit":
-        break
+        elif (user_ch == "rock" and computer_ch == "scissors") or (user_ch == "paper" and computer_ch == "rock") or (user_ch == "scissors" and computer_ch == "paper"):
+            mesage("Won")
 
-    if user_ch not in options:
-        print("not allow")
-        continue
+        else:
+            mesage(" you lose")
 
-    if user_ch == computer_ch:
-        print("tie")
-   
-    elif (user_ch == "rock" and computer_ch == "scissors") or (user_ch == "paper" and computer_ch == "rock") or (user_ch == "scissors" and computer_ch == "paper"):
-        print("Won")
-
-    else:
-        print(" you lose")
+game()
