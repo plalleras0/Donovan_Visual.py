6cbcbfee068b60918d7ea56869934fc76cf4a28a
plalleras0 <gdonovan761@gmail.com>
2024-09-20 11:13:42 -0600

    list

---
+++ listdoc.py
@@ -0,0 +1,52 @@
+#type of list: homegeneus=
+intergers = [1 , 2 , 3 , 4 , 5]
+animal = ["dog", "cat", "goat"]
+name = ["azahel", "Santiago", "Donovan"]
+floats = [2.2, 3.3, 6.7, 10.3]
+#type of lis: heterogeneous=
+number_animels_name = [2, "cat", 34.33, "Travis"]
+#list inside of a list
+list_lists = [(1,2,3), ("cat", "dog", "spider")]
+list = [2, 3, 3.4, 56, 100, 200, 210, 542, 1000]
+#print(list[0])
+
+words = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
+    "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "xigua", "yellow", 
+    "zucchini", "apricot", "blackberry", "cantaloupe", "dragonfruit", "edamame",
+    "flaxseed", "guava", "horseradish", "iceberg", "jackfruit", "kumquat", 
+    "lime", "melon", "nutmeg", "olive", "peach", "pear", "pepper", "plum", 
+    "quinoa", "radish", "spinach", "tomato", "turnip", "walnut", "yam", "zest",
+    "artichoke", "broccoli", "carrot", "daikon", "endive", "fennel", "garlic", 
+    "herb", "jalapeno", "kale", "leek", "mushroom", "nori", "okra", "parsnip",
+    "quail", "rhubarb", "squash", "tarragon", "upland", "veggie", "wasabi", 
+    "xanadu", "yucca", "zinnia", "avocado", "beet", "cabbage", "dill", "escarole", 
+    "figs", "grapefruit", "hops", "jalapenos", "kiwifruit", "lima", "mangosteen", 
+    "nectar", "olive", "pluot", "quesadilla", "romaine", "sorrel", "taro", 
+    "watercress", "ximenia", "yarrow", "zucchini", "aloe", "barley", "clove", 
+    "dulse", "eucalyptus", "fava", "ginseng", "honey", "indigo", "jicama", 
+    "kohlrabi", "lavender", "matcha", "nutrient", "oregano", "parsley", 
+    "quinoa", "rye", "sage", "thyme", "urad", "vanilla", "wheat", "xylitol", 
+    "yogurt", "zest"]
+#print(words[-3])
+
+#list slicing
+list = [3, 4, 7, 80, 34, 67,]
+#print(list[1:4])
+#print(list[1:-1])
+
+#update a list
+
+science = ["art", "chemistry", "guialogy"]
+
+science[0] = "Bialogy"
+#print(science)
+
+intergers = [2, 3, 4, 5,]
+intergers [-1] = 30.5
+#print(intergers)
+#intergers.remove(4)
+#print(intergers)
+#intergers.pop()
+#print(intergers)
