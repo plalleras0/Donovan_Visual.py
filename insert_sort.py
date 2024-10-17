8734ca5a04fff109ad0efa8f8b435a5859dbaffc
plalleras0 <gdonovan761@gmail.com>
2024-10-17 11:16:47 -0600

    sortinsert

---
+++ insert_sort.py
@@ -0,0 +1,32 @@
+def insert():
+    a = ["Master", "online"]
+    si = a.insert(1, "code")
+    print(a)
+
+    Q = [1,2,3,4,5]
+    Q = [6,7,8,9,10]
+    B = [1,2,3,4,5]
+    Q.insert(0, B)
+    print(
+        
+        
+    )
+    print(Q)
+
+def sort():
+    names = ["Alice","Ian","Bob","Charlie","Diana","Ethan","Fiona","George","Hannah","Julia"]
+
+    names.sort()
+    names.sort(reverse=True)
+    print(names)
+        
+    print(sort)
+    print("-----------------------------------------------------------------------------------------")
+    
+    names.sort(reverse=True, key=len)
+    
+    print(names)
+    
+
+sort()
+
